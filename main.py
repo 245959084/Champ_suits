14d46c0a3b440245a7a72e554c0d2f767ab0a129
JingJun <billy.ni222@gmail.com>
2026-06-06 17:03:24 -0700

    add steamlit and add basic data

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from champ-suits!")
+import streamlit as st
+import numpy as np
 
+# display some text
+st.title("Number Doubler")
+st.write("Enter a number and I'll double it for you.")
 
-if __name__ == "__main__":
-    main()
+# ask for input
+number = st.number_input("Enter a number", value=0)
+
+# this is just standard Python
+doubled = number * 2
+
+# display the result
+st.write(f"{number} doubled is {doubled}.")
